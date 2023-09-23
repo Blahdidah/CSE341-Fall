7bf3c117b4f2f82fcd1d515d0b866e5e3eafafeb
Blahdidah <80852722+Blahdidah@users.noreply.github.com>
2023-09-23 12:22:36 -0600

    Updates for week 2, should be working now

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,13 @@
-const routes = require('express').Router();
+//this code tells the system to use contacts
+const express = require('express');
+const router = express.Router();
+
+router.use('/contacts', require('./contacts'))
+
+module.exports = router;
+
+//week1 code
+/*const routes = require('express').Router();
 const lesson1Controller = require('../controllers/lesson1');
 
 routes.get('/', lesson1Controller.melissaRoute);
@@ -7,4 +16,4 @@ routes.get('/sakuraRoute', lesson1Controller.sakuraRoute);
 routes.get('/reiRoute', lesson1Controller.reiRoute);
 routes.get('/ayameRoute', lesson1Controller.ayameRoute);
 
-module.exports = routes;
+module.exports = routes;*/
--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 const express = require('express');
-const connectDB = require('./DB/connection')
+const bodyParser =require('body-parser');
+//const as mongo db instead of connectDB
+const mongodb = require('./DB/connection')
+const port = process.env.port;
 const app = express();
 
-connectDB();
+app
+    .use(bodyParser.json())
+    .use((req,res,next) => {
+        res.setHeader('access-controll-allow-origin','*');
+        next();
+})
+    .use('/', require('./routes'));
 
-const port = 3000;
+mongodb.initDb((err, mongodb) =>{
+    if (err){
+        console.log(err);
+    }else{
+        app.listen(port);
+        console.log(`connected to db and listening on ${port}`);
+        console.log('you got this!');
+        console.log('just a little more testing!')
+    }
+});
+
+
+//depricated from week1
+/*const connectDB = require('./DB/connection')
+connectDB();
 app.use(express.json({extended:false}));
 app.use('/api/user',require('./API/user'));
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
-});
+});*/
